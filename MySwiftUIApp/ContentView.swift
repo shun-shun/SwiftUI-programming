52059e0a136c7914eff1a5f9a315040d9b15708e
権藤　俊 <gondo.syun0711@gmail.com>
2020-08-11 18:07:41 +0900

    Initial Commit

---
+++ MySwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUIApp
+//
+//  Created by しゅん on 2020/08/11.
+//  Copyright © 2020 Shun Gondo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
