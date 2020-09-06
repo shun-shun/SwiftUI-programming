bbc18ef3f926dd50b683154a9a672b758de85f89
権藤　俊 <gondo.syun0711@gmail.com>
2020-09-06 10:53:34 +0900

    p68

--- MySwiftUIApp/ContentView.swift
+++ MySwiftUIApp/ContentView.swift
@@ -10,7 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack{
+                Text("[First]")
+                    .font(.largeTitle)
+//                    .frame(width:150)
+//                    .border(Color.red, width: 2)
+//                    .padding(10)
+                Text("[Second]")
+                .font(.largeTitle)
+//                .frame(width:150)
+//                .border(Color.red, width: 2)
+//                .padding(10)
+                
+            }.frame(width: 300, height: 100)
+                .border(Color.red, width: 2)
+                .padding(10)
+            
+            HStack {
+                Text("[Third]")
+                    .font(.largeTitle)
+//                    .frame(width:150)
+//                    .border(Color.red, width: 2)
+//                    .padding(10)
+                Text("[Fourth]")
+                .font(.largeTitle)
+//                .frame(width:150)
+//                .border(Color.red, width: 2)
+//                .padding(10)
+            }.frame(width: 300, height: 100)
+            .border(Color.red, width: 2)
+            .padding(10)
+        }
+        
+        
     }
 }
 
